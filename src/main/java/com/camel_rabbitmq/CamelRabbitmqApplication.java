332012fe077c35b14600111c66fab50ce8d564e5
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-25 16:16:42 +0700

    remove lib unused.

--- src/main/java/com/camel_rabbitmq/CamelRabbitmqApplication.java
+++ src/main/java/com/camel_rabbitmq/CamelRabbitmqApplication.java
@@ -1,13 +1,12 @@
 package com.camel_rabbitmq;
 
-import com.cloudhopper.smpp.type.SmppChannelException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CamelRabbitmqApplication {
 
-    public static void main(String[] args) throws SmppChannelException {
+    public static void main(String[] args) {
         SpringApplication.run(CamelRabbitmqApplication.class, args);
     }
 
