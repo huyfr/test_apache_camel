a39160addf329ed2659b778a28fa3bf6e54130fa
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-25 16:23:26 +0700

    add new function to add sms to queue.

--- src/main/java/com/camel_rabbitmq/services/transit/impl/TransitSms.java
+++ src/main/java/com/camel_rabbitmq/services/transit/impl/TransitSms.java
@@ -5,12 +5,20 @@ import com.camel_rabbitmq.services.transit.ITransitSms;
 import com.cloudhopper.smpp.pdu.SubmitSm;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+import static com.camel_rabbitmq.ulti.CamelSmppValueHeader.*;
 
 
 @Service
 public class TransitSms implements ITransitSms {
+    private static final Logger logger = LoggerFactory.getLogger(TransitSms.class);
+    HashMap<String, Object> smsHeaders = new HashMap<>();
 
     @Produce(value = "direct:startQueuePoint")
     private final ProducerTemplate producerTemplate;
@@ -30,7 +38,23 @@ public class TransitSms implements ITransitSms {
 
     public void addSmsToQueue(SubmitSm submitSm) {
         try {
-            producerTemplate.asyncSendBody(producerTemplate.getDefaultEndpoint(), submitSm);
+            long endTime = System.currentTimeMillis();
+            setSmsHeaders(submitSm);
+            producerTemplate.sendBodyAndHeaders(producerTemplate.getDefaultEndpoint(), new String (submitSm.getShortMessage()), smsHeaders);
+            logger.info("End time: {}", endTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setSmsHeaders(SubmitSm submitSm) {
+        try {
+            smsHeaders.put("CamelSmppDestAddr", submitSm.getDestAddress().getAddress());
+            smsHeaders.put("CamelSmppDestAddrTon", SMPP_TON_ALNUM);
+            smsHeaders.put("CamelSmppDestAddrNpi", SMPP_NPI_UNK);
+            smsHeaders.put("CamelSmppSourceAddr", submitSm.getSourceAddress().getAddress());
+            smsHeaders.put("CamelSmppSourceAddrTon", SMPP_TON_INTL);
+            smsHeaders.put("CamelSmppSourceAddrNpi", SMPP_NPI_ISDN);
         } catch (Exception e) {
             e.printStackTrace();
         }
