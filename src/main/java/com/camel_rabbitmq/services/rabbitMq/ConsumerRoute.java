03221c4f6ef9532e94a786abdf4f6018e9a89d89
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-31 11:20:08 +0700

    perf(producers, consumers): improves performance
    - Producers: add raw input to queue.
    - Consumers: get raw object from
    queue to SMSC
    
    [skip ci]

--- src/main/java/com/camel_rabbitmq/services/rabbitMq/ConsumerRoute.java
+++ src/main/java/com/camel_rabbitmq/services/rabbitMq/ConsumerRoute.java
@@ -10,12 +10,19 @@ public class ConsumerRoute extends RouteBuilder {
     public void configure() {
 
         try {
-            from("rabbitmq:testExchange?queue=testQueue&autoDelete=false&connectionFactory=#rabbitConnectionFactory")
+            from("rabbitmq:testExchange?" +
+                    "queue=testQueue&" +
+                    "autoDelete=false&" +
+                    "connectionFactory=#rabbitConnectionFactory&" +
+                    "threadPoolSize=3000&" +
+                    "concurrentConsumers=1900")
                     .routeId("RabbitMqConsumer")
                     .log("Get sms from queue with body: ${body}")
-                    .to("smpp://smppclient@localhost:2777?password=password&enquireLinkTimer=3000&transactionTimer=5000&systemType=producer")
-                    .log("Sent sms successful!")
-                    .end();
+                    .to("smpp://smppclient@localhost:2777?" +
+                            "enquireLinkTimer=3000&" +
+                            "transactionTimer=5000&" +
+                            "systemType=producer")
+                    .log("Sent sms successful!");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/camel_rabbitmq/services/rabbitMq/ProducerRoute.java
+++ src/main/java/com/camel_rabbitmq/services/rabbitMq/ProducerRoute.java
@@ -1,6 +1,6 @@
 package com.camel_rabbitmq.services.rabbitMq;
 
-import com.cloudhopper.smpp.pdu.SubmitSm;
+import com.cloudhopper.smpp.pdu.PduRequest;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
@@ -12,14 +12,17 @@ public class ProducerRoute extends RouteBuilder {
     public void configure() {
 
         try {
-            JacksonDataFormat jacksonDataFormat = new JacksonDataFormat(SubmitSm.class);
+            JacksonDataFormat jacksonDataFormat = new JacksonDataFormat(PduRequest.class);
 
             from("direct:startQueuePoint")
                     .id("RabbitMqProducer")
                     .marshal(jacksonDataFormat)
-                    .to("rabbitmq:testExchange?queue=testQueue&autoDelete=false&connectionFactory=#rabbitConnectionFactory")
-                    .log("Sms has entered queue!")
-                    .end();
+                    .to("rabbitmq:testExchange?" +
+                            "queue=testQueue&" +
+                            "autoDelete=false&" +
+                            "connectionFactory=#rabbitConnectionFactory&" +
+                            "channelPoolMaxSize=3000")
+                    .log("Sms has entered queue!");
         } catch (Exception e) {
             e.printStackTrace();
         }
